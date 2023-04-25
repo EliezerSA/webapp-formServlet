e9e1bcced4a1fe782870f1c5fab4a132e4cb032a
EliezerSA <eliezersantiago-100@outlook.com>
2023-04-25 12:47:41 -0600

    Proyecto: webapp-form. Descripcion: Se suben cambios agregando validaciones a formulario en formservlet y se agregan nuevos elemenos en index.html

--- src/main/java/org/ezequiel/apiservlet/webapp/form/FormServlet.java
+++ src/main/java/org/ezequiel/apiservlet/webapp/form/FormServlet.java
@@ -8,6 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -18,6 +21,38 @@ public class FormServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
+        String pais = req.getParameter("pais");
+        String[] lenguajes = req.getParameterValues("lenguajes");
+        String[] roles = req.getParameterValues("roles");
+
+        String idioma = req.getParameter("idioma");
+        boolean habilitar = req.getParameter("habilitar") != null &&
+                req.getParameter("habilitar").equals("on");
+        String secreto = req.getParameter("secreto");
+
+        List<String> errores = new ArrayList<>();
+        if (username == null || username.isBlank()){
+            errores.add("El username es requerido");
+        }
+        if (password == null || password.isBlank()){
+            errores.add("El password no puede ser vacio");
+        }
+        if (email == null || !email.contains("@")){
+            errores.add("El email es requerido y debe tener un formato de correo");
+        }
+        if (pais == null || pais.equals("") || pais.equals(" ")){
+            errores.add("El pais es requerido");
+        }
+        if (lenguajes == null || lenguajes.length == 0){
+            errores.add("Debe seleccionar al menos un tema");
+        }
+        if (roles == null || roles.length == 0){
+            errores.add("Debe seleccionar al menos un rol!");
+        }
+        if (idioma == null){
+            errores.add("Debe selecionar al menos un idioma!");
+        }
+
 
 
         try (PrintWriter out = resp.getWriter()) {
@@ -32,9 +67,31 @@ public class FormServlet extends HttpServlet {
             out.println("         <h1>Resultado form!</h1>");
 
             out.println("         <ul>");
-            out.println("               <li>Username: " + username + "</li>");
-            out.println("               <li>Password: " + password + "</li>");
-            out.println("               <li>Email: " + email + "</li>");
+            if (errores.isEmpty()) {
+                out.println("               <li>Username: " + username + "</li>");
+                out.println("               <li>Password: " + password + "</li>");
+                out.println("               <li>Email: " + email + "</li>");
+                out.println("               <li>Pais: " + pais + "</li>");
+                out.println("               <li>Lenguajes: <ul>");
+                Arrays.asList(lenguajes).forEach(lenguaje -> {
+                    out.println("                <li>" + lenguaje + "</li>");
+                });
+                out.println("               </ul> </li>");
+
+                out.println("               <li>Roles: <ul>");
+                Arrays.asList(roles).forEach(role -> {
+                    out.println("                <li>" + role + "</li>");
+                });
+                out.println("               </ul> </li>");
+                out.println("               <li>Idioma: " + idioma + "</li>");
+                out.println("               <li>Habilitado: " + habilitar + "</li>");
+                out.println("               <li>Secreto: " + secreto + "</li>");
+            }else {
+                errores.forEach(error ->{
+                    out.println("<li>" + error + "</li>");
+                });
+                out.println("<p><a href=\"/webapp-form/index.html\">Regresar</a></p>");
+            }
             out.println("         </ul>");
             out.println("     </body>");
             out.println("</html>");
